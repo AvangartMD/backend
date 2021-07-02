1a08a42f878487337f245d9ed55794f90df61a53
buddana98 <avinash.buddana@minddeft.com>
2021-07-02 18:58:52 +0530

    cron chnages

--- cron/cron.js
+++ cron/cron.js
@@ -3,5 +3,6 @@ const Web3Helper = require('../helper/web3Helper');
 const cron = require('node-cron');
 
 cron.schedule('* * * * *', (req, res) => {
+  console.log('cron called');
   Web3Helper.getPastEvents(req, res);
 });
