26b97282764fc7e5910b65b4e751a4049ab604bc
buddana98 <avinash.buddana@minddeft.com>
2021-07-09 22:21:36 +0530

    web3 chnages

--- helper/web3Helper.js
+++ helper/web3Helper.js
@@ -264,6 +264,7 @@ async function orderEvent(result, order, transactionId) {
         edition: order['amount'],
       });
 
+      console.log('check edition is:', checkEditionAlreadyAdded);
       // check edition added
       if (checkEditionAlreadyAdded) {
         // check it is second hand sale
