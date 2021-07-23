61586db4b66aade07db385126f6eea63fcade761
buddana98 <avinash.buddana@minddeft.com>
2021-07-23 21:17:51 +0530

    web3 helper chnages

--- helper/web3Helper.js
+++ helper/web3Helper.js
@@ -41,7 +41,7 @@ const options = {
 
 const provider =
   process.env.NODE_ENV === 'development'
-    ? 'https://data-seed-prebsc-1-s1.binance.org:8545/'
+    ? 'https://data-seed-prebsc-2-s1.binance.org:8545/'
     : 'https://bsc-dataseed.binance.org/';
 
 const getWeb3Event = {};
