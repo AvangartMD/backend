eb5641f780ab54fb7c6de148d42110dca88ab175
buddana98 <avinash.buddana@minddeft.com>
2021-07-23 17:58:48 +0530

    hall of frame changes

--- cron/cron.js
+++ cron/cron.js
@@ -1,11 +1,16 @@
 const Web3Helper = require('../helper/web3Helper');
+const HallHelper = require('./hallOfFrame');
 
 const cron = require('node-cron');
 
 cron.schedule('* * * * *', (req, res) => {
-  console.log('cron for past events and order called');
   Web3Helper.getPastEvents(req, res);
   Web3Helper.orderBuyedEvent(req, res);
   Web3Helper.getTransferEventFromContract(req, res);
   Web3Helper.getCancelledEvents(req, res);
+});
+
+cron.schedule('0 0 * * *', async (req, res) => {
+  await HallHelper.getArtWorks(req, res);
+  await HallHelper.getTopBuyers(req, res);
 });
--- modules/hallOfFrame/hallOfFrameRoute.js
+++ modules/hallOfFrame/hallOfFrameRoute.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const HallOfFrameHelper = require('../../cron/hallOfFrame');
+const HallCtr = require('./hallOfFrameController');
+const HallMiddleware = require('./hallOfFrameMiddleware');
+
 // const Auth = require('../../helper/auth');
 // const auth = require('../../helper/auth');
 
@@ -12,4 +15,8 @@ hallOfFrameRoute.get('/topArts', topArt);
 // get top collectors
 const topCollectors = [HallOfFrameHelper.getTopBuyers];
 hallOfFrameRoute.get('/topCollector', topCollectors);
+
+// get hall of frame list
+const listHallOfFrame = [HallMiddleware.checkRedis, HallCtr.listHallOfFrame];
+hallOfFrameRoute.get('/list/:type', listHallOfFrame);
 module.exports = hallOfFrameRoute;
