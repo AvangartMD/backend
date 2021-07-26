8527a88ce614f30d7e05b33d7dd7b33a8e696f20
buddana98 <avinash.buddana@minddeft.com>
2021-07-26 13:03:11 +0530

    cancelled order is

--- contract/cancellerOrders.js
+++ contract/cancellerOrders.js
@@ -9,46 +9,49 @@ const Utils = require('../helper/utils');
 const PopularNftModel = require('../modules/admin/popular-nft/popularNftModel');
 
 const fs = require('fs');
+const { resolve } = require('path');
 
 const cancelledEvent = {};
 
 // transfer button
 cancelledEvent.cancelTransfer = async (editionNo, tokenId, transactionHash) => {
-  try {
-    const findNft = await NftModel.findOne({ tokenId: tokenId });
+  return new Promise(async (resolve, reject) => {
+    try {
+      const findNft = await NftModel.findOne({ tokenId: +tokenId });
+      if (findNft) {
+        const findEdition = await EditionModel.findOne({
+          nftId: findNft._id,
+          edition: +editionNo,
+        });
 
-    if (findNft) {
-      const findEdition = await EditionModel.findOne({
-        nftId: tokenId._id,
-        edition: +editionNo,
-      });
+        const saleType = {
+          type: null,
+          price: 0,
+        };
 
-      const saleType = {
-        type: null,
-        price: 0,
-      };
-
-      if (findEdition) {
-        findEdition.isOpenForSale = false;
-        findEdition.saleType = saleType;
-        findEdition.transactionId = transactionHash;
-        await findEdition.save();
+        if (findEdition) {
+          findEdition.isOpenForSale = false;
+          findEdition.saleType = saleType;
+          findEdition.transactionId = transactionHash;
+          await findEdition.save();
+        } else {
+          Utils.echoLog(
+            `Edition not found for ${findNft._id} with edition no ${editionNo}`
+          );
+          resolve(true);
+        }
       } else {
+        // NFT not found
         Utils.echoLog(
-          `Edition not found for ${findNft._id} with edition no ${editionNo}`
+          `NFt not found for ${tokenId} with edition no ${editionNo}`
         );
       }
-    } else {
-      // NFT not found
-      Utils.echoLog(
-        `NFt not found for ${tokenId} with edition no ${editionNo}`
-      );
+      resolve(true);
+    } catch (err) {
+      Utils.echoLog(`Error in cancelTransfer event ${err} `);
+      resolve(false);
     }
-    return true;
-  } catch (err) {
-    Utils.echoLog(`Error in cancelTransfer event ${err} `);
-    return false;
-  }
+  });
 };
 
 module.exports = cancelledEvent;
