8518aa86f6eb6e5018ef25ba8f2afa5551ed39fc
buddana98 <avinash.buddana@minddeft.com>
2021-09-01 16:55:33 +0530

    profile schema chnages

--- modules/admin/profile-info/profileInfoModel.js
+++ modules/admin/profile-info/profileInfoModel.js
@@ -1,35 +1,45 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
 const profileInfoSchema = new Schema(
-    {
-        url: {
-            type: String,
-            required: true,
-        },
-        banner: {
-            en: {
-                type: String,
-                required: true,
-            },
-            tu: {
-                type: String,
-                required: true,
-            },
-        },
-        isActive: {
-            type: Boolean,
-            default: true,
-        },
+  {
+    url: {
+      type: String,
+      required: true,
     },
+    banner: {
+      en: {
+        type: String,
+        required: true,
+      },
+      tu: {
+        type: String,
+        required: true,
+      },
+    },
+    mobile: {
+      en: {
+        type: String,
+        required: true,
+      },
+      tu: {
+        type: String,
+        required: true,
+      },
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
 
-    {
-        timestamps: true,
-        toJSON: {
-            getters: true,
-        },
-    }
+  {
+    timestamps: true,
+    toJSON: {
+      getters: true,
+    },
+  }
 );
 
-module.exports = mongoose.model("profileInfo", profileInfoSchema);
+module.exports = mongoose.model('profileInfo', profileInfoSchema);
