56f0472eb456d25cee23203be23d40752b2819f1
buddana98 <avinash.buddana@minddeft.com>
2021-09-10 11:23:08 +0530

    profile info modal chnages

--- modules/admin/profile-info/profileInfoModel.js
+++ modules/admin/profile-info/profileInfoModel.js
@@ -2,32 +2,35 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
+const decryptProperty = function (value) {
+  if (value) {
+    return `${process.env.IPFSURL}/${value}`;
+  } else {
+    return null;
+  }
+};
+
+const imageSchmea = new Schema({
+  en: {
+    type: String,
+    required: true,
+    get: decryptProperty,
+  },
+  tu: {
+    type: String,
+    required: true,
+    get: decryptProperty,
+  },
+});
+
 const profileInfoSchema = new Schema(
   {
     url: {
       type: String,
       required: true,
     },
-    banner: {
-      en: {
-        type: String,
-        required: true,
-      },
-      tu: {
-        type: String,
-        required: true,
-      },
-    },
-    mobile: {
-      en: {
-        type: String,
-        required: true,
-      },
-      tu: {
-        type: String,
-        required: true,
-      },
-    },
+    banner: imageSchmea,
+    mobile: imageSchmea,
     isActive: {
       type: Boolean,
       default: true,
