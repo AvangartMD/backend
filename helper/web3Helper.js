d42b5e74eb344637303a46f9904dbb94a646d9c1
buddana98 <avinash.buddana@minddeft.com>
2021-09-22 17:30:33 +0530

    web hlerp changes

--- helper/web3Helper.js
+++ helper/web3Helper.js
@@ -44,7 +44,7 @@ const options = {
 
 const provider =
   process.env.NODE_ENV === 'development'
-    ? 'https://data-seed-prebsc-2-s1.binance.org:8545/'
+    ? 'https://bsc-dataseed.binance.org/'
     : 'https://bsc-dataseed.binance.org/';
 
 const getWeb3Event = {};
