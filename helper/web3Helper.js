0de3011797e04f0be1ee5244f60f06db1c9ab64e
buddana98 <avinash.buddana@minddeft.com>
2021-11-02 13:34:00 +0530

    source changes

--- helper/web3Helper.js
+++ helper/web3Helper.js
@@ -74,7 +74,7 @@ getWeb3Event.getTransferEvent = async (req, res) => {
         //   from: '0x0000000000000000000000000000000000000000', //,
         //   // to: "0x8c8Ea652DE618a30348dCce6df70C8d2925E6814"
         // },
-        // fromBlock: 11130135,
+        fromBlock: 12298700,
       })
       .on('data', async (getPastEvents) => {
         // console.log('getPastEvents', getPastEvents);
